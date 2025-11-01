3624e9cb98d05505c906c683137c4066b12d45bf
Muhammad Alvito Madisyahputra <muhammadalvito2@gmail.com>
2025-11-01 23:49:22 +0700

    Add maven pipeline

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,31 @@
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage("Clean") {
+                steps {
+                    script {
+                        maven(["clean"])
+                    }
+                }
+            }
+
+            stage("Compile") {
+                steps {
+                    script {
+                        maven(["compile"])
+                    }
+                }
+            }
+
+            stage("Test") {
+                steps {
+                    script {
+                        maven(["test"])
+                    }
+                }
+            }
+        }
+    }
+}
